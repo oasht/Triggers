2d524c40c55ce9dc4f1a15ecb1ad3f1b65a8f6bf
Olga Shtoppel <olgashtoppel@gmail.com>
2023-06-11 14:47:15 +0300

    Добавьте файлы проекта.

---
+++ Counter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triggers_hw
+{
+    public class Counter
+    {
+
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int SerialID { get; set; }
+        public DateTime lastCheck { get; set; }
+        public bool hotWater { get; set; }
+    }
+}
